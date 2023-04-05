360c1716d2adc77d5fe59ba8fe839ad3953e8519
Insanis <78218644+insanisartist@users.noreply.github.com>
2023-04-05 23:34:47 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup # импортируем библиотеку BeautifulSoup
+import requests # импортируем библиотеку requests
+
+def parse():
+    d = dict()
+    k = int()
+    text = str()
+    url = 'https://m.imdb.com/chart/top/?ref_=nv_mv_250' # передаем необходимы URL адрес
+    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+    soup.get_text(strip=True)
+    #print(soup)
+    mydivs = soup.find_all("span", {"class": "media-body media-vertical-align"})
+    for data in mydivs:
+        k+=1
+        text = data.text.strip()
+        d[k]=text
+        print(d[k])
+        #print("След")
+    print("fixed")
+
+if __name__ == '__main__':
+    parse()
