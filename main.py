3e8554a03c7e9763ba1fd57215b4b4506f2d51a3
Insanis <78218644+insanisartist@users.noreply.github.com>
2023-04-20 00:02:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ def parse():
     page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
     soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
     soup.get_text(strip=True)
+    #print(soup)
     mydivs = soup.find_all("span", {"class": "media-body media-vertical-align"})
     for data in mydivs:
         k+=1
         text = data.text.strip()
+        text = text.replace("\n", " ")
+        text = text.replace("\r", " ")
+        text = text.replace("    ", " Рейтинг: ")
         d[k]=text
         print(d[k])
+        #print("След")
 
 if __name__ == '__main__':
     parse()
